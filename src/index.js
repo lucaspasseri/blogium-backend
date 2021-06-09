d23cc672ad51633f9047ad91d0c4d4293d3e4fab
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2021-06-09 19:41:26 -0300

    Get posts done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+
+const postsList = [{
+    id: 1,
+    title: 'Hello World',
+    coverUrl: 'https://miro.medium.com/max/1024/1*OohqW5DGh9CQS4hLY5FXzA.png',
+    contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
+    content: 'Este é o conteúdo do post, o que realmente vai aparecer na página do post...',
+    commentCount: 2
+  }];
+
 app.get("/", (req, res) => {
-    res.send("OK");
+    res.send(postsList);
 });
 
 app.listen(4001);
